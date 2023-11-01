c196ecbdcf89a5937531af32d8a7209f95c31904
Habib <habibbehdarvandi.dev@gmail.com>
2023-11-01 16:31:56 -0700

    Authenticate on Login Page and redirect to Home.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,72 @@
 import Header from "@/components/Header/Header";
 import React, { useState } from "react";
 import styles from "@/pages/styles/index.module.css";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, notification } from "antd";
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 
-const onFinish = (values: any) => {
-  console.log("Success:", values);
-};
 
-const login = (props) => {
+
+
+
+const login = () => {
 
   const [form] = Form.useForm();
+  const router = useRouter();
+
+  const onFinish = async (values: any) => {
+    console.log("Success:", values);
+
+    const reqBody = {
+      username: values.username,
+      password: values.password
+    }
+
+    try {
+      const response = await axios.post(`/api/auth/login`, reqBody);
+
+      if (response.status === 400) {
+        console.log(response);
+        notification.error({
+          message: `Invalid Context`,
+          description:
+            'Please make sure you fill all the fields',
+          duration: 0,
+        })
+      } else if (response.status === 404) {
+        console.log(response);
+        notification.error({
+          message: `User Not Found!`,
+          description:
+            'User with this Username is not in our Database! Please SignIn First.',
+          duration: 0,
+        })
+      } else if (response.status === 403) {
+        console.log(response);
+        notification.error({
+          message: `Invalid username or password.`,
+          description:
+            'Please Check the Username or Password',
+          duration: 0,
+        })
+      } else if (response.status === 200) {
+
+        router.push('/home');
+      }
+
+    } catch (error) {
+
+    }
+
+
+  };
 
 
   return (
     <>
       <div className={styles.bg}>
-
-
-
 
         <Form form={form} name="horizontal_login" layout="vertical" onFinish={onFinish} className={styles.form_bg}>
           <h2 className={styles.header_title}>
